9cbec601fffe4a11757656106f0fb4f7138f8fa0
MBF-720 <mohamedbenfredj8@gmail.com>
2024-05-23 17:04:04 +0100

    add activity ModifyNote

---
+++ app/src/main/java/pfe/africar/activitys/ModifyNote.java
@@ -0,0 +1,16 @@
+package pfe.africar.activitys;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import pfe.africar.R;
+
+public class ModifyNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_note);
+    }
+}
