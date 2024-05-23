9d7a9fec5b8284be6980ad395285634e0291e0b0
Medox666 <issamouhamed66@gmail.com>
2024-05-23 14:27:11 +0100

    grade

--- app/src/main/java/pfe/africar/activitys/grade_sheet_activity.java
+++ app/src/main/java/pfe/africar/activitys/grade_sheet_activity.java
@@ -18,62 +18,37 @@
 	package pfe.africar.activitys;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import pfe.africar.R;
 
 	public class grade_sheet_activity extends Activity {
-
-	
 	private View _bg__grade_sheet_ek2;
 	private TextView grade_sheet_ek3;
-	private View _bg__frame_29_ek1;
+
 	private ImageView ellipse_36_ek1;
 	private TextView flen_el_fouleni_ek4;
-	private ImageView x_1_ek13;
-	private TextView pass_mark_ek1;
-	private TextView math_ek6;
-	private TextView phy;
-	private TextView sci;
-	private TextView geo;
-	private TextView his;
-	private TextView ds1;
-	private TextView ds1_ek1;
-	private TextView _10;
-	private TextView _10_ek1;
-	private TextView ds2;
-	private TextView ds2_ek1;
-	private TextView _10_ek2;
-	private TextView __;
-	private TextView exam;
-	private TextView exam_ek1;
-	private TextView _10_ek3;
-	private TextView ___ek1;
-	private TextView test;
-	private TextView test_ek1;
-	private TextView moy;
-	private TextView moy_ek1;
-	private TextView _10_ek4;
-	private TextView ___ek2;
-	private TextView _10_ek5;
-	private TextView ___ek3;
-	private TextView _15_42_ek1;
-	private ImageView line_18;
-	private ImageView line_19;
-	private ImageView line_20;
-	private ImageView line_21;
-	private ImageView line_22;
-	private ImageView line_23;
-	private ImageView ep_arrow_up_bold_1;
-	private ImageView ep_arrow_up_bold_2;
-	private ImageView ep_arrow_up_bold_3;
-	private ImageView ep_arrow_up_bold_4;
-	private ImageView ep_arrow_up_bold_5;
 	private ImageView rectangle_32_ek13;
 	private TextView updates;
 	private TextView stats;
@@ -83,6 +58,11 @@ import pfe.africar.R;
 	private ImageView vector_ek644;
 	private ImageView vector_ek645;
 	private ImageView vector_ek646;
+		private ListView gradesListView;
+		private Button addGradeButton;
+		private List<String> gradesList;
+		private ArrayAdapter<String> adapter;
+		private FirebaseFirestore db;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -90,51 +70,12 @@ import pfe.africar.R;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.grade_sheet);
 
-		
 		_bg__grade_sheet_ek2 = (View) findViewById(R.id._bg__grade_sheet_ek2);
 		grade_sheet_ek3 = (TextView) findViewById(R.id.grade_sheet_ek3);
-		_bg__frame_29_ek1 = (View) findViewById(R.id._bg__frame_29_ek1);
+
 		ellipse_36_ek1 = (ImageView) findViewById(R.id.ellipse_36_ek1);
 		flen_el_fouleni_ek4 = (TextView) findViewById(R.id.flen_el_fouleni_ek4);
-		x_1_ek13 = (ImageView) findViewById(R.id.x_1_ek13);
-		pass_mark_ek1 = (TextView) findViewById(R.id.pass_mark_ek1);
-		math_ek6 = (TextView) findViewById(R.id.math_ek6);
-		phy = (TextView) findViewById(R.id.phy);
-		sci = (TextView) findViewById(R.id.sci);
-		geo = (TextView) findViewById(R.id.geo);
-		his = (TextView) findViewById(R.id.his);
-		ds1 = (TextView) findViewById(R.id.ds1);
-		ds1_ek1 = (TextView) findViewById(R.id.ds1_ek1);
-		_10 = (TextView) findViewById(R.id._10);
-		_10_ek1 = (TextView) findViewById(R.id._10_ek1);
-		ds2 = (TextView) findViewById(R.id.ds2);
-		ds2_ek1 = (TextView) findViewById(R.id.ds2_ek1);
-		_10_ek2 = (TextView) findViewById(R.id._10_ek2);
-		__ = (TextView) findViewById(R.id.__);
-		exam = (TextView) findViewById(R.id.exam);
-		exam_ek1 = (TextView) findViewById(R.id.exam_ek1);
-		_10_ek3 = (TextView) findViewById(R.id._10_ek3);
-		___ek1 = (TextView) findViewById(R.id.___ek1);
-		test = (TextView) findViewById(R.id.test);
-		test_ek1 = (TextView) findViewById(R.id.test_ek1);
-		moy = (TextView) findViewById(R.id.moy);
-		moy_ek1 = (TextView) findViewById(R.id.moy_ek1);
-		_10_ek4 = (TextView) findViewById(R.id._10_ek4);
-		___ek2 = (TextView) findViewById(R.id.___ek2);
-		_10_ek5 = (TextView) findViewById(R.id._10_ek5);
-		___ek3 = (TextView) findViewById(R.id.___ek3);
-		_15_42_ek1 = (TextView) findViewById(R.id._15_42_ek1);
-		line_18 = (ImageView) findViewById(R.id.line_18);
-		line_19 = (ImageView) findViewById(R.id.line_19);
-		line_20 = (ImageView) findViewById(R.id.line_20);
-		line_21 = (ImageView) findViewById(R.id.line_21);
-		line_22 = (ImageView) findViewById(R.id.line_22);
-		line_23 = (ImageView) findViewById(R.id.line_23);
-		ep_arrow_up_bold_1 = (ImageView) findViewById(R.id.ep_arrow_up_bold_1);
-		ep_arrow_up_bold_2 = (ImageView) findViewById(R.id.ep_arrow_up_bold_2);
-		ep_arrow_up_bold_3 = (ImageView) findViewById(R.id.ep_arrow_up_bold_3);
-		ep_arrow_up_bold_4 = (ImageView) findViewById(R.id.ep_arrow_up_bold_4);
-		ep_arrow_up_bold_5 = (ImageView) findViewById(R.id.ep_arrow_up_bold_5);
+
 		rectangle_32_ek13 = (ImageView) findViewById(R.id.rectangle_32_ek13);
 		updates = (TextView) findViewById(R.id.updates);
 		stats = (TextView) findViewById(R.id.stats);
@@ -144,11 +85,55 @@ import pfe.africar.R;
 		vector_ek644 = (ImageView) findViewById(R.id.vector_ek644);
 		vector_ek645 = (ImageView) findViewById(R.id.vector_ek645);
 		vector_ek646 = (ImageView) findViewById(R.id.vector_ek646);
-	
-		
+		gradesListView = findViewById(R.id.gradesListView);
+		addGradeButton = findViewById(R.id.addGradeButton);
+
+		db = FirebaseFirestore.getInstance();
+		gradesList = new ArrayList<>();
+		adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, gradesList);
+		gradesListView.setAdapter(adapter);
+
+		loadGrades();
+		addGradeButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Intent to start AddGradeActivity
+				Intent intent = new Intent(grade_sheet_activity.this, AddGradeActivity.class);
+				startActivity(intent);
+			}
+		});
 		//custom code goes here
-	
+		gradesListView.setOnItemClickListener((parent, view, position, id) -> {
+			// Handle click event to modify or delete grade
+			String selectedGrade = gradesList.get(position);
+			Intent intent = new Intent(grade_sheet_activity.this, EditGradeActivity.class);
+			intent.putExtra("gradeId", selectedGrade);
+			startActivity(intent);
+		});
 	}
-}
+
+
+		private void loadGrades() {
+			db.collection("grades").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+				@Override
+				public void onComplete(@NonNull Task<QuerySnapshot> task) {
+					if (task.isSuccessful()) {
+						gradesList.clear();
+						for (DocumentSnapshot document : task.getResult()) {
+							String grade = document.getString("grade");
+							gradesList.add(grade);
+						}
+						adapter.notifyDataSetChanged();
+					} else {
+						Toast.makeText(grade_sheet_activity.this, "Failed to load grades", Toast.LENGTH_SHORT).show();
+					}
+				}
+			});
+		}
+
+
+
+	}
+
 	
 	
