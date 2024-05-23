6cb462e1ce7fdc5e52ea37e689a1c26842c6f49d
MBF-720 <mohamedbenfredj8@gmail.com>
2024-05-23 14:51:32 +0100

    creation des classe et activiter pour medox

---
+++ app/src/main/java/pfe/africar/activitys/AddGradeActivity.java
@@ -0,0 +1,16 @@
+package pfe.africar.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pfe.africar.R;
+
+public class AddGradeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_grade);
+    }
+}
---
+++ app/src/main/java/pfe/africar/activitys/EditGradeActivity.java
@@ -0,0 +1,16 @@
+package pfe.africar.activitys;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import pfe.africar.R;
+
+public class EditGradeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_grade);
+    }
+}
